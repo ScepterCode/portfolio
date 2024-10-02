f67287c2ead7f05a4fafbb56d2ca44baae9c17fb
keldan24 <kelechidaniel171kd@gmail.com>
2024-10-02 08:55:40 +0100

    commit for deployment

--- public/script.js
+++ public/script.js
@@ -45,3 +45,46 @@ anchorTags.forEach(anchorTag => {
         event.preventDefault();
     });
 });
+
+document.getElementById('myForm').addEventListener('submit', sendEmail);
+function sendEmail(e) {
+    e.preventDefault(); // Uncomment this if you're using this function in a form submission context
+
+    const nameInput = document.getElementById('name');
+    const name = nameInput.value
+    .split(' ') // Split the string into words
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize each word
+    .join(' ');
+    const email = document.getElementById('email').value;
+    const subject = document.getElementById('subject').value;
+    const message = document.getElementById('message').value;
+
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "keldan40k@gmail.com",
+        Password: "E59B0231169F477C0576AF0D35830B0BA3F9",
+        To: 'keldan40k@gmail.com',
+        From: 'keldan40k@gmail.com',
+        Subject: subject,
+        Body: `Name: ${name} <br>Email: ${email} <br>Message: ${message}`
+    }).then(
+        message => {
+            if (message == 'OK') {
+                Swal.fire({
+                    title: "Success",
+                    text: "Message sent successfully",
+                    icon: "success"
+                  });
+        }
+        else(
+            Swal.fire({
+                title: "Error",
+                text: "An error occurred while sending the message, please use a valid email address",
+                icon: "error"
+            })
+        )
+    }
+    )
+
+    this.reset()
+}
